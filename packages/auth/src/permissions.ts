dc0fce702b37fe658208a8b5c081e1ae7bf9ab4a
Fredson <fredsouza.dev@gmail.com>
2026-05-19 17:59:09 -0400

    add permissions by position

---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,18 @@
+import type { AbilityBuilder } from '@casl/ability'
+import type { AppAbility } from '.'
+import type { User } from './models/user'
+
+type Role = 'ADMIN' | 'MANAGER' | 'EMPLOYEE'
+
+type PermissionsByRole = (
+  user: User,
+  builder: AbilityBuilder<AppAbility>
+) => void
+
+export const permissions: Record<Role, PermissionsByRole> = {
+  ADMIN(_, { can }) {
+    can('manage', 'all')
+  },
+  MANAGER() {},
+  EMPLOYEE() {},
+}
